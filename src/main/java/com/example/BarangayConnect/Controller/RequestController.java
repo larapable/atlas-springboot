2a5137cb770432bee1c254b88194fd7693a9f61e
larapable <lara.pable@cit.edu>
2023-11-29 21:38:23 +0800

    updet

---
+++ src/main/java/com/example/BarangayConnect/Controller/RequestController.java
@@ -0,0 +1,47 @@
+package com.example.BarangayConnect.Controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.BarangayConnect.Entity.RequestEntity;
+import com.example.BarangayConnect.Service.RequestService;
+
+@RestController
+@RequestMapping("/requests")
+@CrossOrigin(origins = "http://localhost:3000")
+public class RequestController {
+    @Autowired
+    RequestService rserv;
+
+    @PostMapping("/insertRequest")
+    public RequestEntity insertRequest(@RequestBody RequestEntity request) {
+        return rserv.insertRequest(request);
+    }
+
+    @GetMapping("/getAllRequest")
+    public List<RequestEntity> getAllRequest() {
+        return rserv.getAllRequest();
+    }
+
+    @PutMapping("/updateRequest") 
+    public RequestEntity updateRequest(@RequestParam int docid, @RequestBody RequestEntity newRequestDetails) {
+        return rserv.updateRequest(docid, newRequestDetails);
+    }
+
+    @DeleteMapping("/deleteRequest/{docid}")
+    public String deleteRequest(@PathVariable int docid) {
+        return rserv.deleteRequest(docid);
+    }
+
+}
---
+++ src/main/java/com/example/BarangayConnect/Repository/RequestRepository.java
@@ -0,0 +1,9 @@
+package com.example.BarangayConnect.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.BarangayConnect.Entity.RequestEntity;
+
+public interface RequestRepository extends JpaRepository<RequestEntity,Integer> {
+    
+}
